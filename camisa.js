2c0a407bcae62fba209cb1b6c6b1b511a8926e85
Marc-58 <mqueralt16@gmail.com>
2025-06-14 19:01:38 +0200

    Update and rename camisa.js to

--- camisa.js
+++ camisa.js
@@ -1,7 +1,1 @@
-function dibuixaCamisa(pit, llarg) {
-  // Dibuix simplificat
-  stroke(0);
-  fill(200);
-  rect(100, 100, pit, llarg);
-  line(100 + pit / 2, 100, 100 + pit / 2, 100 + llarg); // centre
-}
+
