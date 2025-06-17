2db7e47e156b84cfd29f0ba6bca06ceb5fa72c22
Marc-58 <mqueralt16@gmail.com>
2025-06-17 15:31:46 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,14 +74,14 @@ function dibuixaPatro(p) {
       p.line(pinçaX, 10, pinçaX + 1.5 * escala, 10 + 10 * escala);
       p.line(pinçaX + 3 * escala, 10, pinçaX + 1.5 * escala, 10 + 10 * escala);
 
-      p.line(200, 10, 200, llarg);
-      p.line(200, llarg+10, 200 + cadera / 4 - 1*escala, llarg+10);
-      p.line(200, 10, 200 + cintura / 4 - 1*escala + 3 * escala, 10);
-      p.line(200 + cintura / 4 - 1*escala + 3 * escala, 10, 200 + cadera / 4 - escala, 10+18 * escala);
-      p.line(200 + cadera / 4 - escala, 10+18 * escala, 200 + cadera / 4 - escala,10+ llarg);
+      p.line(20+cadera/4, 10, 20+cadera/4, llarg);
+      p.line(20+cadera/4, llarg+10, 20+cadera/4 + cadera / 4 - 1*escala, llarg+10);
+      p.line(20+cadera/4, 10, 20+cadera/4 + cintura / 4 - 1*escala + 3 * escala, 10);
+      p.line(20+cadera/4 + cintura / 4 - 1*escala + 3 * escala, 10, 20+cadera/4 + cadera / 4 - escala, 10+18 * escala);
+      p.line(20+cadera/4 + cadera / 4 - escala, 10+18 * escala, 20+cadera/4 + cadera / 4 - escala,10+ llarg);
 
       // Pinces
-      const pinçay = 20+cadera + cintura / 8;
+      const pinçay = 20+cadera/4 + cintura / 8;
       p.line(pinçay, 10, pinçay + 1.5 * escala, 10 + 10 * escala);
       p.line(pinçay + 3 * escala, 10, pinçay + 1.5 * escala, 10 + 10 * escala);
     }
