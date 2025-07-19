a608dfd2ab7e814137752ae74f6825b343f06e18
Marc-58 <mqueralt16@gmail.com>
2025-07-19 14:11:40 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -103,7 +103,7 @@ function dibuixaPatro(p) {
 
       p.rect(10, 10, espatlles / 2, talleEspatlles);
       p.line(10 + coll / 6, 10, 10 + espatlles / 2, 10 + (talleEspatlles - caiguda));//hombro
-      p.line(10 + torax / 4, talleEspatlles - caiguda + sisa + 10,  10 + (cintura / 4) + 2 * escala , 10 + talleEspatlles);//igual
+p.line(10 + torax / 4, talleEspatlles - caiguda + sisa + 10, 10 + cintura / 4 + 2 * escala, 10 + talleEspatlles); //igual CORREGIDA
       p.line(10 + cintura / 8 + 1.5 * escala, talleEspatlles - caiguda + sisa + 10, 10 + (cintura / 8), 10 + talleEspatlles);//pinca
       p.line(10 + cintura / 8 + 1.5 * escala, talleEspatlles - caiguda + sisa + 10, 10 + (cintura / 8) + 3*escala, 10 + talleEspatlles);//pinca
       p.line(10, 10 + talleEspatlles, 10 + (cintura / 4) + 2 * escala, 10 + talleEspatlles);//cintura
