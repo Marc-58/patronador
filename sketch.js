c6a24cb0f80b447aa5a52adab1efad2979f61393
Marc-58 <mqueralt16@gmail.com>
2025-07-30 01:40:46 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,8 +59,20 @@ function dibuixaPatro(p) {
     p.noFill();
 
     if (tipus === "faldilla") {
-      // El teu codi faldilla...
-    }
+const cintura = mides.cintura * escala;
+      const cadera = mides.cadera * escala;
+      const llarg = mides.llarg * escala;
+      const marge = 10;
+
+      p.line(marge, marge, marge, marge + llarg);
+      p.line(marge, marge + llarg, marge + cadera / 4 + escala, marge + llarg);
+      p.line(marge, marge, marge + cintura / 4 + 4 * escala, marge);
+      p.line(marge + cintura / 4 + 4 * escala, marge, marge + cadera / 4 + escala, marge + 18 * escala);
+      p.line(marge + cadera / 4 + escala, marge + 18 * escala, marge + cadera / 4 + escala, marge + llarg);
+
+      const pinçaX1 = marge + cintura / 8;
+      p.line(pinçaX1, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);
+      p.line(pinçaX1 + 3 * escala, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);    }
     else if (tipus === "brusa") {
       const espatlles = mides.espatllesTotal * escala;
       const talleEspatlles = mides.talleEspatlles * escala;
