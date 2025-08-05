fe0eff5cfaba4c54e1ab82422af0f12a02be71f5
Marc-58 <mqueralt16@gmail.com>
2025-08-05 21:46:03 +0200

    Update and rename patroBrusa.js to patroCosACintura.js

--- patroCosACintura.js
+++ patroCosACintura.js
@@ -1,4 +1,4 @@
-function dibuixarPatroBrusa(p, mides, escala) {
+function dibuixarPatroCosACintura(p, mides, escala) {
       const espatlles = mides.espatllesTotal * escala;
       const talleEspatlles = mides.talleEspatlles * escala;
       const coll = mides.coll * escala;
