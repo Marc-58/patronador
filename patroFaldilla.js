5dbd2d22be00c413834e453cc802fd3e312eab37
Marc-58 <mqueralt16@gmail.com>
2025-08-05 02:43:09 +0200

    Update patroFaldilla.js

--- patroFaldilla.js
+++ patroFaldilla.js
@@ -1,30 +1,31 @@
 function dibuixarPatroFaldilla(p, mides,escala) {
 
 const cintura = mides.cintura * escala;
-  const cadera = mides.cadera * escala;
-  const llarg = mides.llarg * escala;
-  const marge = 10;
+      const cadera = mides.cadera * escala;
+      const llarg = mides.llarg * escala;
+      const marge = 10;
 
-  p.line(marge, marge, marge, marge + llarg);
-  p.line(marge, marge + llarg, marge + cadera / 4 + escala, marge + llarg);
-  p.line(marge, marge, marge + cintura / 4 + 4 * escala, marge);
-  p.line(marge + cintura / 4 + 4 * escala, marge, marge + cadera / 4 + escala, marge + 18 * escala);
-  p.line(marge + cadera / 4 + escala, marge + 18 * escala, marge + cadera / 4 + escala, marge + llarg);
+      p.line(marge, marge, marge, marge + llarg);
+      p.line(marge, marge + llarg, marge + cadera / 4 + escala, marge + llarg);
+      p.line(marge, marge, marge + cintura / 4 + 4 * escala, marge);
+      p.line(marge + cintura / 4 + 4 * escala, marge, marge + cadera / 4 + escala, marge + 18 * escala);
+      p.line(marge + cadera / 4 + escala, marge + 18 * escala, marge + cadera / 4 + escala, marge + llarg);
 
-  const pinçaX1 = marge + cintura / 8;
-  p.line(pinçaX1, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);
-  p.line(pinçaX1 + 3 * escala, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);
+      const pinçaX1 = marge + cintura / 8;
+      p.line(pinçaX1, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);
+      p.line(pinçaX1 + 3 * escala, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);
 
-  const sep = 150;
+
+  //const sep = 150;
  
 
-  p.line(sep, marge, sep, marge + llarg);
-  p.line(sep, marge + llarg, sep + cadera / 4 + escala, marge + llarg);
-  p.line(sep, marge, sep + cintura / 4 + 2 * escala, marge);
-  p.line(sep + cintura / 4 + 2 * escala, marge, sep + cadera / 4 + escala, marge + 18 * escala);
-  p.line(sep + cadera / 4 + escala, marge + 18 * escala, sep + cadera / 4 + escala, marge + llarg);
+  //p.line(sep, marge, sep, marge + llarg);
+  //p.line(sep, marge + llarg, sep + cadera / 4 + escala, marge + llarg);
+  //p.line(sep, marge, sep + cintura / 4 + 2 * escala, marge);
+  //p.line(sep + cintura / 4 + 2 * escala, marge, sep + cadera / 4 + escala, marge + 18 * escala);
+  //p.line(sep + cadera / 4 + escala, marge + 18 * escala, sep + cadera / 4 + escala, marge + llarg);
 
-  const pinçaX2 = sep + cintura / 8 + escala;
-  p.line(pinçaX2, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala);
-  p.line(pinçaX2 + 3 * escala, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala);
+  //const pinçaX2 = sep + cintura / 8 + escala;
+ // p.line(pinçaX2, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala);
+//  p.line(pinçaX2 + 3 * escala, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala);
 } 
