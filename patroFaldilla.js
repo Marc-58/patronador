6f66354a815d10cc7636b40773f8ef8661c39a06
Marc-58 <mqueralt16@gmail.com>
2025-08-18 15:51:56 +0200

    Update patroFaldilla.js

--- patroFaldilla.js
+++ patroFaldilla.js
@@ -1,29 +1,35 @@
-function dibuixarPatroFaldilla(p, mides,escala) {
+function dibuixarPatroFaldilla(p, mides, escala) {
+    const cintura = mides.cintura * escala;
+    const cadera = mides.cadera * escala;
+    const llarg = mides.llarg * escala;
+    const marge = 10;
 
-const cintura = mides.cintura * escala;
-      const cadera = mides.cadera * escala;
-      const llarg = mides.llarg * escala;
-      const marge = 10;
+    // Dibuix del patró de la faldilla
+    // Dibuixar línies verticals i horitzontals per marcar la forma
+    p.line(marge, marge, marge, marge + llarg); // línia esquerra
+    p.line(marge, marge + llarg, marge + cadera / 4 + escala, marge + llarg); // línia inferior
+    p.line(marge, marge, marge + cintura / 4 + 4 * escala, marge); // línia superior esquerra
+    p.line(marge + cintura / 4 + 4 * escala, marge, marge + cadera / 4 + escala, marge + 18 * escala); // línia superior dreta
+    p.line(marge + cadera / 4 + escala, marge + 18 * escala, marge + cadera / 4 + escala, marge + llarg); // línia dreta
 
-      p.line(marge, marge, marge, marge + llarg);
-      p.line(marge, marge + llarg, marge + cadera / 4 + escala, marge + llarg);
-      p.line(marge, marge, marge + cintura / 4 + 4 * escala, marge);
-      p.line(marge + cintura / 4 + 4 * escala, marge, marge + cadera / 4 + escala, marge + 18 * escala);
-      p.line(marge + cadera / 4 + escala, marge + 18 * escala, marge + cadera / 4 + escala, marge + llarg);
+    // Dibuixar pinces a la cintura
+    const pinçaX1 = marge + cintura / 8;
+    p.line(pinçaX1, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala); // línia de pinça esquerra
+    p.line(pinçaX1 + 3 * escala, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala); // línia de pinça dreta
 
-      const pinçaX1 = marge + cintura / 8;
-      p.line(pinçaX1, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);
-      p.line(pinçaX1 + 3 * escala, marge, pinçaX1 + 1.5 * escala, marge + 10 * escala);
+    // Separació entre les dues parts de la faldilla
+    const sep = marge + cadera / 4 + 100;
 
-  const sep = marge + cadera / 4 + 100;
+    // Dibuixar la segona part de la faldilla
+    p.line(sep, marge, sep, marge + llarg); // línia esquerra
+    p.line(sep, marge + llarg, sep + cadera / 4 + escala, marge + llarg); // línia inferior
+    p.line(sep, marge, sep + cintura / 4 + 2 * escala, marge); // línia superior esquerra
+    p.line(sep + cintura / 4 + 2 * escala, marge, sep + cadera / 4 + escala, marge + 18 * escala); // línia superior dreta
+    p.line(sep + cadera / 4 + escala, marge + 18 * escala, sep + cadera / 4 + escala, marge + llarg); // línia dreta
 
-  p.line(sep, marge, sep, marge + llarg);
-  p.line(sep, marge + llarg, sep + cadera / 4 + escala, marge + llarg);
-  p.line(sep, marge, sep + cintura / 4 + 2 * escala, marge);
-  p.line(sep + cintura / 4 + 2 * escala, marge, sep + cadera / 4 + escala, marge + 18 * escala);
-  p.line(sep + cadera / 4 + escala, marge + 18 * escala, sep + cadera / 4 + escala, marge + llarg);
+    // Dibuixar pinces a la segona part de la cintura
+    const pinçaX2 = sep + cintura / 8 + escala;
+    p.line(pinçaX2, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala); // línia de pinça esquerra
+    p.line(pinçaX2 + 3 * escala, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala); // línia de pinça dreta
+}
 
-  const pinçaX2 = sep + cintura / 8 + escala;
-  p.line(pinçaX2, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala);
-  p.line(pinçaX2 + 3 * escala, marge, pinçaX2 + 1.5 * escala, marge + 10 * escala);
-} 
