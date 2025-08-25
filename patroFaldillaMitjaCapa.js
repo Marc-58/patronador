718e8dfd3c7d912d545061ae044d12c374c3bd15
Marc-58 <mqueralt16@gmail.com>
2025-08-25 11:51:29 +0200

    Update patroFaldillaMitjaCapa.js

--- patroFaldillaMitjaCapa.js
+++ patroFaldillaMitjaCapa.js
@@ -2,9 +2,13 @@ function dibuixarPatroFaldillaMitjaCapa(p, mides, escala) {
     const cintura = mides.cintura * escala;
     const llarg = mides.llarg * escala;
     const marge = 10;
-    const radi =   cintura/2*3.14;
 
-arc(marge+llarg+radi, marge, llarg+radi, llarg+radi, PI, THREE_PI); // x, y, ample, alt
-arc(marge+llarg+radi, marge,radi, radi, PI, THREE_PI); // x, y, ample, alt
-p.line(marge, marge, marge +llarg+radi/2, marge); // línia esquerra
+    const radi = cintura / 2 * Math.PI; 
 
+    // Dibuixar arcs
+    p.arc(marge + llarg + radi, marge, llarg + radi, llarg + radi, p.PI, p.TWO_PI + p.PI); 
+    p.arc(marge + llarg + radi, marge, radi, radi, p.PI, p.TWO_PI + p.PI); 
+
+    // Línia esquerra
+    p.line(marge, marge, marge + llarg + radi / 2, marge);
+}
