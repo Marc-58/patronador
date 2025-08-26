8fcc73921eb0170c0bbac34045a73674c86f53c9
Marc-58 <mqueralt16@gmail.com>
2025-08-27 00:48:01 +0200

    Update patroPantalo.js

--- patroPantalo.js
+++ patroPantalo.js
@@ -1,1 +1,9 @@
+function dibuixarPatroPantalo(p, mides, escala) {
+    const cintura = mides.cintura * escala;
+    const cadera = mides.cadera * escala;
+    const llarg = mides.llarg * escala;
+    const tiro = mides.tiro * escala;
+    const genoll = mides.genoll * escala;
+    const marge = 10;
 
+   p.rect(marge, marge, cadera / 4, llarg);
