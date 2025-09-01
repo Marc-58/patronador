63935e99ae0f772fad11e72fbde6fec3fb9e591a
Marc-58 <mqueralt16@gmail.com>
2025-09-01 20:50:19 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -177,15 +177,6 @@ function dibuixaPatro(p) {
   };
 }
 
-function dibuixarPatroFaldillaCapaSencera(p, mides, escala) {
-    const cintura = mides.cintura * escala;
-    const llarg = mides.llarg * escala;
-    const marge = 10;
-    const radi =   cintura/2*3.14;
-
-p.arc(marge+llarg+radi/2, marge, llarg+radi/2, llarg+radi/2, PI, TWO_PI); // x, y, ample, alt
-p.arc(marge+llarg+radi/2, marge,radi/2, radi/2, PI, TWO_PI); // x, y, ample, alt
-p.line(marge, marge, marge +2*llarg+radi, marge); // línia esquerra
 
 function descarregarCanvas() {
   const canvases = document.getElementsByTagName("canvas");
