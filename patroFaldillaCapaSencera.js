6e02c09a97489879fc64f117dcf744a5adc849b5
Marc-58 <mqueralt16@gmail.com>
2025-09-08 11:24:23 +0200

    Update patroFaldillaCapaSencera.js

--- patroFaldillaCapaSencera.js
+++ patroFaldillaCapaSencera.js
@@ -1,9 +1,8 @@
-function dibuixarPatroFaldillaCapaSencera(p, mides, escala) {
-    const cintura = mides.cintura * escala;
-    const llarg = mides.llarg * escala;
-    const marge = 10;
-    const radi =   cintura/2*3.14;
+ const cintura = mides.cintura * escala;
+      const llarg = mides.llarg * escala;
+     const radi = cintura / (2 * Math.PI);
 
-p.arc(marge+llarg+radi/2, marge, llarg+radi/2, llarg+radi/2, PI, TWO_PI); // x, y, ample, alt
-p.arc(marge+llarg+radi/2, marge,radi/2, radi/2, PI, TWO_PI); // x, y, ample, alt
-p.line(marge, marge, marge +2*llarg+radi, marge); // línia esquerra
+    // Arc de cintura (semicercle cap avall)
+    p.arc(marge + radi+llarg , marge , 2 *radi ,2*radi, 0, p.PI);
+    p.arc(marge + radi+llarg , marge , 2 * radi+llarg*2, 2 * radi+llarg*2, 0, p.PI);
+    p.line(marge, marge, marge + 2 * radi*2+llarg*2, marge );
