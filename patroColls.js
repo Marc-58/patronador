510cea54edd9cd56c7bb7d1159fa7171ac9f695f
Marc-58 <mqueralt16@gmail.com>
2025-09-08 11:25:32 +0200

    Update patroColls.js

--- patroColls.js
+++ patroColls.js
@@ -1,1 +1,8 @@
+const coll = mides.coll * escala;
+      
+      p.rect(marge, marge, coll/2, 6*escala);
+      
+      p.line(marge+coll/2-1*escala,marge, marge+coll/2, marge+5*escala );
+      p.line(marge+coll/6,marge+6*escala, marge+coll/2, marge+5*escala );
+
 
